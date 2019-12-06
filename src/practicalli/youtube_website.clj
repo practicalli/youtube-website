4daed753533cd43a0d0198b5242a770a20b44bb7
Practicalli <john@practical.li>
2019-12-06 21:29:38 +0000

    Update dependencies, added require to main namespace
    Added latest versions of ring, compojure, clj-http and clojure.data.json
    libraries.
    
    Required the namespaces of these libraries into the main youtube-website
    namespace.

--- src/practicalli/youtube_website.clj
+++ src/practicalli/youtube_website.clj
@@ -1,5 +1,12 @@
 (ns practicalli.youtube-website
-  (:gen-class))
+  (:gen-class)
+  (:require [ring.adapter.jetty :as adapter]
+            [ring.util.response :refer [response]]
+            [compojure.core     :refer [defroutes GET POST]]
+            [compojure.route    :refer [not-found]]
+            [clj-http.client    :as http-client]
+            [clojure.data.json  :as json]))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
